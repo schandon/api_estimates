836b09ed09c855f467468626650d23d4afa03fa3
schandon <sp.alexandre26@gmail.com>
2025-07-23 11:03:08 -0300

    Updated routes on validation by zod

--- src/http/routes/get-all-service-orders.ts
+++ src/http/routes/get-all-service-orders.ts
@@ -1,28 +1,43 @@
 import { PrismaClient } from '@prisma/client';
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
-
-// import { z } from "zod/v4";
+import { z } from 'zod/v4';
 
 const prisma = new PrismaClient();
 
 export const getServiceOrder: FastifyPluginCallbackZod = (app) => {
-  app.get('/api/v1/os', async (request, reply) => {
+  app.get('/api/v1/os', async (_request, reply) => {
     try {
       const result = await prisma.service_orders.findMany();
       return result;
     } catch (error) {
+      // biome-ignore lint/suspicious/noConsole: Erro em Tela
+      console.log('Error na Rota:', error);
       return reply.status(400).send({ message: 'Error: OS not found' });
     }
   });
-  app.get('/api/v1/os/:OrdemServico', async (request, reply) => {
-    try {
-      const { OrdemServico } = request.params as { OrdemServico: string };
-      const result = await prisma.service_orders.findUnique({
-        where: { OrdemServico },
-      });
-      return result;
-    } catch (error) {
-      return reply.status(400).send({ message: 'Error IMO: Vessel not found' });
+  app.get(
+    '/api/v1/os/:OrdemServico',
+    {
+      schema: {
+        params: z.object({
+          OrdemServico: z.string().nonempty('OS não pode ser Vazio'),
+        }),
+      },
+    },
+    async (request, reply) => {
+      try {
+        const { OrdemServico } = request.params;
+        const result = await prisma.service_orders.findUnique({
+          where: { OrdemServico },
+        });
+        return result;
+      } catch (error) {
+        // biome-ignore lint/suspicious/noConsole: Erro em Tela
+        console.log('Error na Rota:', error);
+        return reply
+          .status(400)
+          .send({ message: 'Error IMO: Vessel not found' });
+      }
     }
-  });
+  );
 };
--- src/http/routes/get-all-vessels.ts
+++ src/http/routes/get-all-vessels.ts
@@ -1,12 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
-
-// import { z } from "zod/v4";
+import { z } from 'zod/v4';
 
 const prisma = new PrismaClient();
 
 export const getVessel: FastifyPluginCallbackZod = (app) => {
-  app.get('/api/v1/vessel', async (request, reply) => {
+  app.get('/api/v1/vessel', async (_request, reply) => {
     try {
       const result = await prisma.vessel.findMany({
         select: {
@@ -19,36 +18,88 @@ export const getVessel: FastifyPluginCallbackZod = (app) => {
         },
       });
       return result;
-    } catch (error) {
+    } catch {
       return reply
         .status(400)
         .send({ message: 'Error GET: Vessels not found' });
     }
   });
 
-  app.get('/api/v1/vessel/imo/:imo', async (request, reply) => {
-    try {
-      const { imo } = request.params as { imo: string };
-      const result = await prisma.vessel.findFirst({
-        where: { imo },
-      });
-      return result;
-    } catch (error) {
-      return reply.status(400).send({ message: 'Error IMO: Vessel not found' });
+  app.get(
+    '/api/v1/vessel/imo/:imo',
+    {
+      schema: {
+        params: z.object({
+          imo: z.string(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      try {
+        const { imo } = request.params;
+        const result = await prisma.vessel.findFirst({
+          where: { imo },
+        });
+        return result;
+      } catch {
+        return reply
+          .status(400)
+          .send({ message: 'Error IMO: Vessel not found' });
+      }
     }
-  });
+  );
 
-  app.get('/api/v1/vessel/mmsi/:mmsi', async (request, reply) => {
-    try {
-      const { mmsi } = request.params as { mmsi: string };
-      const result = await prisma.vessel.findFirst({
-        where: { mmsi },
-      });
-      return result;
-    } catch (error) {
-      return reply
-        .status(400)
-        .send({ message: 'Error MMSI: Vessel not found' });
+  app.get(
+    '/api/v1/vessel/mmsi/:mmsi',
+    {
+      schema: {
+        params: z.object({
+          mmsi: z.string(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      try {
+        const { mmsi } = request.params as { mmsi: string };
+        const result = await prisma.vessel.findFirst({
+          where: { mmsi },
+        });
+        return result;
+      } catch (error) {
+        // biome-ignore lint/suspicious/noConsole: Erro em Tela
+        console.log('Error na Rota:', error);
+        return reply
+          .status(400)
+          .send({ message: 'Error MMSI: Vessel not found' });
+      }
     }
-  });
+  );
+  app.get(
+    '/api/v1/vessel/:embarcacao',
+    {
+      schema: {
+        params: z.object({
+          embarcacao: z
+            .string()
+            .nonempty('ID não pode ser Vazio')
+            // biome-ignore lint/performance/useTopLevelRegex: Regex de Ajuste do código
+            .regex(/^\d+$/, 'MMSI deve conter apenas dígitos')
+            .transform((val) => Number.parseInt(val, 10))
+            // biome-ignore lint/suspicious/noGlobalIsNan: Função de validação valor NaN
+            .refine((val) => !isNaN(val), 'ID deve ser um número valido'),
+        }),
+      },
+    },
+    async (request, reply) => {
+      try {
+        const { embarcacao } = request.params;
+        const result = await prisma.vessel.findUnique({
+          where: { embarcacao },
+        });
+        return result;
+      } catch {
+        return reply.status(400).send({ message: 'Error: Vessel not found' });
+      }
+    }
+  );
 };
